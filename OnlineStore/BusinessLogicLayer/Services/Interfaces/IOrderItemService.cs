cc1b3a84a05ee283019328a3193ff2ec8542ec1e
Alexey <paaworker@gmail.com>
2025-02-22 14:47:58 +0300

    Created the IOrderItemService interface

---
+++ OnlineStore/BusinessLogicLayer/Services/Interfaces/IOrderItemService.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLogicLayer.Services.DTOs;
+
+namespace BusinessLogicLayer.Services.Interfaces
+{
+    public interface IOrderItemService
+    {
+        Task<OrderItemDto> GetOrderItemByIdAsync(int orderItemId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<OrderItemDto>> GetOrderItemsByOrderIdAsync(int orderId, CancellationToken cancellationToken = default);
+        Task<OrderItemDto> CreateOrderItemAsync(OrderItemDto newOrderItem, CancellationToken cancellationToken = default);
+        Task UpdateOrderItemAsync(int orderItemId, OrderItemDto updatedOrderItem, CancellationToken cancellationToken = default);
+        Task DeleteOrderItemAsync(int orderItemId, CancellationToken cancellationToken = default);
+    }
+}

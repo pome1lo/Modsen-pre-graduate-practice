2a29ebfa0cb1766e5c931b64992f08999f805dd0
Alexey <paaworker@gmail.com>
2025-02-25 21:48:29 +0300

    Created the UserService class

--- OnlineStore/BusinessLogicLayer/Services/DTOs/UserDto.cs
+++ OnlineStore/BusinessLogicLayer/Services/DTOs/UserDto.cs
@@ -1,6 +1,22 @@
-﻿namespace BusinessLogicLayer.Services.DTOs
+﻿using DataAccessLayer.Models;
+
+namespace BusinessLogicLayer.Services.DTOs
 {
     public class UserDto
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public UserDto() { }
+
+        public UserDto(Users user)
+        {
+            Id = user.Id;
+            Username = user.Username;
+            Password = user.Password;
+            Email = user.Email;
+        }
     }
 }
---
+++ OnlineStore/BusinessLogicLayer/Services/UserService.cs
@@ -0,0 +1,76 @@
+﻿using BusinessLogicLayer.Exceptions;
+using BusinessLogicLayer.Services.DTOs;
+using BusinessLogicLayer.Services.Interfaces;
+using DataAccessLayer.Data.Repositories;
+using DataAccessLayer.Models;
+
+namespace BusinessLogicLayer.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDto> GetUserByIdAsync(int userId, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with ID {userId} not found");
+            }
+            return new UserDto(user);
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllUsersAsync(CancellationToken cancellationToken = default)
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+            return users.Select(u => new UserDto(u));
+        }
+
+        public async Task<UserDto> CreateUserAsync(UserDto newUser, CancellationToken cancellationToken = default)
+        {
+            var user = new Users
+            {
+                Username = newUser.Username,
+                Email = newUser.Email,
+                Password = newUser.Password,
+                CreatedDate = DateTime.UtcNow,
+                UpdatedDate = DateTime.UtcNow
+            };
+
+            await _userRepository.AddAsync(user);
+            return new UserDto(user);
+        }
+
+        public async Task UpdateUserAsync(int userId, UserDto updatedUser, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with ID {userId} not found");
+            }
+
+            user.Username = updatedUser.Username;
+            user.Email = updatedUser.Email;
+            user.Password = updatedUser.Password;
+            user.UpdatedDate = DateTime.UtcNow;
+
+            await _userRepository.UpdateAsync(user);
+        }
+
+        public async Task DeleteUserAsync(int userId, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+            {
+                throw new NotFoundException($"User with ID {userId} not found");
+            }
+
+            await _userRepository.DeleteAsync(userId);
+        }
+    }
+}

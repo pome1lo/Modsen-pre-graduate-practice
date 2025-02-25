a3b7c2de51e220154c0c349cb2fb126c18c1f647
Ilya <2003pesetsky@mail.ru>
2025-02-25 19:42:09 +0300

    T-8: add business logic validator

---
+++ OnlineStore/BusinessLogicLayer/Services/Validator/BusinessException.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessLogicLayer.Services.Validator
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+        }
+
+        public BusinessException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ OnlineStore/BusinessLogicLayer/Services/Validator/BusinessLogicValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessLogicLayer.Services.Validator;
+
+public static class BusinessLogicValidator
+{
+    /// <summary>
+    /// If the condition is not true, error message will be returned
+    /// </summary>
+    /// <param name="condition">condition: 4 > 5</param>
+    /// <param name="errorMessage">Error message</param>
+    /// <exception cref="BusinessException"></exception>
+    public static void Validate (bool condition, string errorMessage)
+    {
+        if (!condition)
+            throw new BusinessException(errorMessage);
+    }
+}

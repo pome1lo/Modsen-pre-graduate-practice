29dff34f1185783647b96e99486f3d2eedc98ab5
pome1lo <paaworker@gmail.com>
2025-03-17 13:24:14 +0300

    Create Footer component

--- OnlineStore/clientui/src/App.jsx
+++ OnlineStore/clientui/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <div>
                 INDEX
             </div>
+            <Footer/>
         </>
     )
 }
